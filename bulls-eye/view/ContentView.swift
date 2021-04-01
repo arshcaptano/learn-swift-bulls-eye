47f68763c0a2349ce5e611539941f82d46d9abb4
arsh <arshfodcc@gmail.com>
2021-04-01 15:33:28 +0300

    UI extract views

--- bulls-eye/view/ContentView.swift
+++ bulls-eye/view/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var isAlertVisible = false
     @State private var sliderValue = 50.0
     @State private var game = Game()
@@ -18,58 +17,75 @@ struct ContentView: View {
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
-                    .bold()
-                    .kerning(2.0)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4.0)
-                    .font(.footnote)
-                    .foregroundColor(Color("TextColor"))
-                Text(String(game.target))
-                    .fontWeight(.black)
-                    .font(.largeTitle)
-                    .foregroundColor(Color("TextColor"))
-                    .kerning(-1.0)
-                HStack {
-                    Text("1")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                    Slider(value: $sliderValue, in: 1.0...100.0)
-                    Text("100")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                }
-                .padding()
-                Button(action: {
-                    isAlertVisible = true
-                }) {
-                    Text("Hit me".uppercased())
-                        .bold()
-                        .font(.title3)
-                }
-                .padding(20.0)
-                .background(
-                    ZStack {
-                        Color("ButtonColor")
-                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
-                                       startPoint: .top,
-                                       endPoint: .bottom)
-                    }
-                )
-                .foregroundColor(.white)
-                .cornerRadius(21.0)
-                .alert(
-                    isPresented: $isAlertVisible,
-                    content: {
-                        let roundedValue = Int(sliderValue.rounded())
-                        return Alert(
-                            title: Text("Hello"),
-                            message: Text("The slider's value is \(roundedValue).\n" +
-                                            "You scored \(game.points(sliderValue: roundedValue)) points in this round."),
-                            dismissButton: .default(Text("Close")))
-                    })
+                InstructionView(game: $game)
+                SliderView(sliderValue: $sliderValue)
+                HitMeButton(game: $game, sliderValue: $sliderValue, isAlertVisible: $isAlertVisible)
             }
         }
+    }
+}
+
+
+struct InstructionView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack{
+            InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            BigNumberText(text: String(game.target))
+        }
+    }
+}
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            SliderLabelText(text: "100")
+        }
+        .padding()
+    }
+}
+
+struct HitMeButton: View {
+    @Binding var game: Game
+    @Binding var sliderValue: Double
+    @Binding var isAlertVisible: Bool
+    
+    var body: some View {
+        Button(action: {
+            isAlertVisible = true
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
+                               startPoint: .top,
+                               endPoint: .bottom)
+            }
+        )
+        .foregroundColor(.white)
+        .cornerRadius(21.0)
+        .alert(
+            isPresented: $isAlertVisible,
+            content: {
+                let roundedValue = Int(sliderValue.rounded())
+                return Alert(
+                    title: Text("Hello"),
+                    message: Text("The slider's value is \(roundedValue).\n" +
+                                    "You scored \(game.points(sliderValue: roundedValue)) points in this round."),
+                    dismissButton: .default(Text("Close")))
+            })
     }
 }
 
---
+++ bulls-eye/view/TextViews.swift
@@ -0,0 +1,54 @@
+//
+//  TextViews.swift
+//  bulls-eye
+//
+//  Created by Arshfod Captano on 01/04/2021.
+//
+
+import SwiftUI
+
+struct InstructionText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text.uppercased())
+            .bold()
+            .kerning(2.0)
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.footnote)
+            .foregroundColor(Color("TextColor"))
+    }
+}
+
+struct BigNumberText: View {
+    var text: String
+    
+    var body: some View {
+        Text(String(text))
+            .fontWeight(.black)
+            .font(.largeTitle)
+            .foregroundColor(Color("TextColor"))
+            .kerning(-1.0)
+    }
+}
+
+struct SliderLabelText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .foregroundColor(Color("TextColor"))
+    }
+}
+
+struct TextViews_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            InstructionText(text: "Instructions")
+            BigNumberText(text: "100")
+            SliderLabelText(text: "1")
+        }
+    }
+}
